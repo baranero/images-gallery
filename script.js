27a18172e560302f8e70b9d2c2a06b54a2c6d26f
baranero <jakub.baran2@gmail.com>
2023-09-15 18:16:21 +0200

    event gallery

--- script.js
+++ script.js
@@ -9,19 +9,19 @@ const carouselControlNext = document.querySelector(".carousel-control-next");
 const carouselControlPrev = document.querySelector(".carousel-control-prev");
 let scrollPosition = 0;
 const carouselEvent = document.getElementById("imageCarouselEvent");
-const carouselEventInner = document.querySelector(
-  ".image-event-carousel-inner"
-);
+const carouselEventInner = document.querySelector(".image-event-carousel-inner");
+const activeImageContainer = document.querySelector(".active-image-container")
+const activeImageDescriptionContainer = document.querySelector(".active-image-description-container")
 
-// Loop through the imagesArray and create img elements for each image
+// Display first section - grid image gallery
 for (let i = 0; i < imagesArray.length; i++) {
   const image = imagesArray[i];
 
   const imgWrapper = document.createElement("div");
-  imgWrapper.classList.add("position-relative", "p-2", "img-wrapper"); // Add the 'image-wrapper' class
+  imgWrapper.classList.add("position-relative", "p-2", "img-wrapper");
 
   const img = document.createElement("img");
-  img.src = image.src; // Set the src attribute from the imagesArray
+  img.src = image.src;
   img.alt = image.alt;
 
   const button = document.createElement("button");
@@ -39,11 +39,9 @@ for (let i = 0; i < imagesArray.length; i++) {
   );
 
   button.appendChild(icon);
-
   imgWrapper.appendChild(img);
   imgWrapper.appendChild(button);
 
-  // Determine which row to add the image to based on the index
   if (i === 0) {
     imgWrapper.classList.add("col-lg-7");
     firstRow.appendChild(imgWrapper);
@@ -58,19 +56,16 @@ for (let i = 0; i < imagesArray.length; i++) {
 
 // Modal code
 
-// Create the close button dynamically
 const closeButton = document.createElement("button");
 closeButton.type = "button";
 closeButton.classList.add("btn-close");
 closeButton.setAttribute("data-bs-dismiss", "modal");
 closeButton.setAttribute("aria-label", "Close");
 
-// Add an event listener to handle modal closure when the close button is clicked
 closeButton.addEventListener("click", function () {
-  modalImage.removeAttribute("src"); // Clear the modal image
+  modalImage.removeAttribute("src");
 });
 
-// Append the close button to the modal header
 const modalHeader = document.querySelector(".modal-header");
 modalHeader.appendChild(closeButton);
 
@@ -80,7 +75,6 @@ buttons.forEach((button) => {
     const thumbnail = this.parentNode.querySelector("img");
     const thumbnailSrc = thumbnail.getAttribute("src");
 
-    // Set the modal image source to the thumbnail source
     modalImage.setAttribute("src", thumbnailSrc);
   });
 });
@@ -92,12 +86,35 @@ function pauseCarousel() {
   }
 }
 
-// Call the pauseCarousel function to stop the automatic slide
 pauseCarousel();
 
-// Create and populate the carousel items dynamically
+// Display second section - image slider
+for (let i = 0; i < 6; i++) {
+  const image = imagesArray[i];
 
-// Define a function to initialize the carousel after it's populated
+  const carouselItem = document.createElement("div");
+  if (image.id === 2) {
+    carouselItem.classList.add("carousel-item", "active", "col-lg-4");
+  } else {
+    carouselItem.classList.add("carousel-item", "col-lg-4");
+  }
+
+  const imgWrapper = document.createElement("div");
+  imgWrapper.classList.add(
+    "d-flex",
+    "justify-content-center",
+    "align-items-center",
+    "image-carousel-wrapper"
+  );
+
+  const img = document.createElement("img");
+  img.src = image.src;
+  img.alt = image.alt;
+
+  carouselItem.appendChild(imgWrapper);
+  imgWrapper.appendChild(img);
+  carouselInner.appendChild(carouselItem);
+}
 
 carouselControlNext.addEventListener("click", function () {
   const cardWidth = document
@@ -140,44 +157,14 @@ carousel.addEventListener("slid.bs.carousel", function () {
   const carouselItems = carouselInner.querySelectorAll(".carousel-item");
   const lastItem = carouselItems[carouselItems.length - 1];
 
-  // Check if it's the last slide
   if (lastItem.classList.contains("active")) {
-    // Move the first slide to the end
     const firstItem = carouselItems[0];
     carouselInner.appendChild(firstItem.cloneNode(true));
     carouselInner.removeChild(firstItem);
   }
 });
 
-// Loop through the imagesArray and create img elements for each image
-for (let i = 0; i < 6; i++) {
-  const image = imagesArray[i];
-
-  const carouselItem = document.createElement("div");
-  if (image.id === 2) {
-    carouselItem.classList.add("carousel-item", "active", "col-lg-4");
-  } else {
-    carouselItem.classList.add("carousel-item", "col-lg-4");
-  }
-
-  const imgWrapper = document.createElement("div");
-  imgWrapper.classList.add(
-    "d-flex",
-    "justify-content-center",
-    "align-items-center",
-    "image-carousel-wrapper"
-  );
-
-  const img = document.createElement("img");
-  img.src = image.src; // Set the src attribute from the imagesArray
-  img.alt = image.alt;
-
-  carouselItem.appendChild(imgWrapper);
-  imgWrapper.appendChild(img);
-  carouselInner.appendChild(carouselItem);
-}
-
-// Iterate through imagesArray and create carousel items
+// Display third section - event gallery
 for (let i = 0; i < imagesArray.length; i++) {
   const image = imagesArray[i];
 
@@ -201,6 +188,8 @@ for (let i = 0; i < imagesArray.length; i++) {
   img.src = image.src;
   img.alt = image.alt;
 
+  activeImageContainer.innerHTML = `<div class="active-image-description-container"><p class="event-image-title">${imagesArray[0].alt}</p></div>`
+
   // Add a click event listener to imgWrapper elements
   imgWrapper.addEventListener("click", () => {
     // Remove the 'active' class from the currently active item
@@ -212,7 +201,8 @@ for (let i = 0; i < imagesArray.length; i++) {
 
     // Update the active image container with the new active image
     const newActiveImage = carouselItem.querySelector("img").cloneNode(true);
-    activeImageContainer.innerHTML = ""; // Clear the container
+    activeImageContainer.innerHTML = `<div class="active-image-description-container"><p class="event-image-title">${newActiveImage.getAttribute("alt")}</p></div>`
+
     activeImageContainer.appendChild(newActiveImage); // Append the new active image
   });
 
@@ -221,13 +211,22 @@ for (let i = 0; i < imagesArray.length; i++) {
   carouselEventInner.appendChild(carouselItem);
 }
 
+
+
 // Create a container for the active image
-const activeImageContainer = document.createElement("div");
-activeImageContainer.classList.add("active-image-container");
 
 // Find the active image within the carousel
 const activeImage = carouselEventInner.querySelector(".carousel-item.active img").cloneNode(true);
 
+// function addImageTitle() {
+//   activeImageDescriptionContainer.innerHTML = ""
+//   const imgTitleContent = activeImage.getAttribute("alt");
+//   console.log(imgTitleContent);
+//   const imgTitle = document.createElement('p')
+//   imgTitle.textContent = imgTitleContent
+//   activeImageDescriptionContainer.appendChild(imgTitle)
+//   imgTitle.classList.add('event-image-title')
+// }
 // Append the active image to the container
 activeImageContainer.appendChild(activeImage);
 
@@ -241,22 +240,29 @@ function updateActiveImage() {
   // Find the active image within the carousel
   const activeImage = carouselEventInner.querySelector(".carousel-item.active img").cloneNode(true);
   console.log(activeImage);
+  // Get the alt text of the active image
 
   // Clear the activeImageContainer and append the new active image
-  activeImageContainer.innerHTML = "";
+  activeImageContainer.innerHTML = `<div class="active-image-description-container"><p class="event-image-title">${activeImage.getAttribute("alt")}</p></div>`;
   activeImageContainer.appendChild(activeImage);
+  console.log(activeImageContainer);
+
+
 }
 
 nextButton.addEventListener("click", () => {
   const currentActiveItem = carouselEventInner.querySelector(".carousel-item.active");
   let nextItem = currentActiveItem.nextElementSibling;
+  
 
   if (!nextItem) {
 
     nextItem = carouselEventInner.querySelector(".carousel-item:first-child");
   }
 
+
   updateActiveImage();
+
 });
 
 prevButton.addEventListener("click", () => {
@@ -269,4 +275,5 @@ prevButton.addEventListener("click", () => {
   }
 
   updateActiveImage();
+
 });
